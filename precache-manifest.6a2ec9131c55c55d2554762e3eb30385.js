9d8608c34c534cea61bd3a82e7361656b5486892
Vaibhav Dhoke <vaibhavdhoke1@gmail.com>
2020-08-02 18:56:04 -0400

    Updates

---
+++ precache-manifest.6a2ec9131c55c55d2554762e3eb30385.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff6b1acf16747a8e28f9af416f3fb015",
+    "url": "/reciepe_search_app/index.html"
+  },
+  {
+    "revision": "5b57dd40bbd22193bcae",
+    "url": "/reciepe_search_app/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "adf2f6bb945eccc755dd",
+    "url": "/reciepe_search_app/static/css/main.89298acf.chunk.css"
+  },
+  {
+    "revision": "5b57dd40bbd22193bcae",
+    "url": "/reciepe_search_app/static/js/2.e6399b24.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/reciepe_search_app/static/js/2.e6399b24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "adf2f6bb945eccc755dd",
+    "url": "/reciepe_search_app/static/js/main.a9dfd436.chunk.js"
+  },
+  {
+    "revision": "5f8257677b706e742023",
+    "url": "/reciepe_search_app/static/js/runtime-main.aaf159aa.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reciepe_search_app/static/media/logo.5d5d9eef.svg"
+  }
+]);
